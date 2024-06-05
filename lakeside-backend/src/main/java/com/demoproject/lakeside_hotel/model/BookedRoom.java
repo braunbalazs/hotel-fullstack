0d139a7fa7300383905a777ba45844fe247d9ef1
Balazs Braun <102475215+braunbalazs@users.noreply.github.com>
2024-06-05 10:51:22 +0800

    Set up docker db

--- lakeside-backend/src/main/java/com/demoproject/lakeside_hotel/model/BookedRoom.java
+++ lakeside-backend/src/main/java/com/demoproject/lakeside_hotel/model/BookedRoom.java
@@ -1,4 +1,64 @@
 package com.demoproject.lakeside_hotel.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class BookedRoom {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "check_in")
+    private LocalDate checkInDate;
+
+    @Column(name = "check_out")
+    private LocalDate checkOutDate;
+
+    private String guestFullName;
+    private String guestEmail;
+
+    @Column(name = "adults")
+    private Integer numOfAdults;
+
+    @Column(name = "children")
+    private Integer numOfChildren;
+
+    @Column(name = "total_guests")
+    private Integer totalNumOfGuests;
+
+    @Column(name = "confirmation_code")
+    private String bookingConfirmationCode;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id")
+    private Room room;
+
+    public void calculateTotalNumOfGuests() {
+        this.totalNumOfGuests = this.numOfAdults + this.numOfChildren;
+    }
+
+    public void setNumOfAdults(Integer numOfAdults) {
+        this.numOfAdults = numOfAdults;
+        calculateTotalNumOfGuests();
+    }
+
+    public void setNumOfChildren(Integer numOfChildren) {
+        this.numOfChildren = numOfChildren;
+        calculateTotalNumOfGuests();
+    }
+
+    public void setBookingConfirmationCode(String bookingConfirmationCode) {
+        this.bookingConfirmationCode = bookingConfirmationCode;
+    }
 }
